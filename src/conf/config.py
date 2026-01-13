641045d47c8c748e6a85c272949bfd24d3fdb7d0
ntigkaris <87975862+ntigkaris@users.noreply.github.com>
2026-01-14 00:21:46 +0200

    Added filenames

--- src/conf/config.py
+++ src/conf/config.py
@@ -6,7 +6,8 @@ class cfg:
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
     imgdir = '../data/captions'
     recdir = '../data/snapshots'
-    tgtdir = 'Downloads'
+    imgfilename = f'IMG{timestamp}.png'
+    recfilename = f'REC{timestamp}.txt'
     imgsize = 128
     kernel = np.array([[-1,-1,-1],
                        [-1,9,-1],
