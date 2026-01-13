85ae1ee7e8cccd87f19cd3b3f127314b2e414183
ntigkaris <87975862+ntigkaris@users.noreply.github.com>
2026-01-14 00:22:34 +0200

    Renamed imported functions

--- src/cerberus.py
+++ src/cerberus.py
@@ -1,6 +1,6 @@
-from utils.helpers import takeSnapshot,logContents
+from utils.helpers import takeCaption,logHomeContents
 from conf.config import cfg
 
 if __name__=='__main__':
-    takeSnapshot(imageSize=cfg.imgsize,kernel=cfg.kernel)
-    logContents(query=cfg.tgtdir)
+    takeCaption()
+    logHomeContents()
