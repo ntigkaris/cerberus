b20682142083edcd96bcc39d474d6e8361368f15
ntigkaris <87975862+ntigkaris@users.noreply.github.com>
2026-01-14 16:52:24 +0200

    Added temp dir

--- src/conf/config.py
+++ src/conf/config.py
@@ -6,6 +6,7 @@ class cfg:
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
     imgdir = '../data/captions'
     recdir = '../data/snapshots'
+    tmpdir = '../data/temp'
     imgsize = 256
     kernel = np.array([[-1,-1,-1],
                        [-1,9,-1],
