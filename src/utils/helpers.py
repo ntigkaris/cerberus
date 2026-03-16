30c83cf22fcebf9782125c9f26b3291cb2371e82
ntigkaris <87975862+ntigkaris@users.noreply.github.com>
2026-03-16 21:19:12 +0200

    added cleanup period - enhancement on cleanup function

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -7,39 +7,43 @@ import cv2
 
 from conf.config import cfg
 
-def cleanUp(oldDir):
-    os.makedirs(name=cfg.tmpdir,exist_ok=True)
-    oldID = (datetime.date.today()-datetime.timedelta(days=1)).strftime('%Y%m%d')
-    for path in pathlib.Path(oldDir).rglob(pattern=f'*{oldID}*'):
-        os.rename(src=path.__str__(),
-                  dst=path.__str__().replace('\\','/')\
-                                    .replace(oldDir,cfg.tmpdir))
-    if len(os.listdir(path=cfg.tmpdir)): shutil.make_archive(base_name=f'{oldDir}/{oldID}',
-                                                             format='zip',
-                                                             root_dir=cfg.tmpdir)
-    shutil.rmtree(path=cfg.tmpdir)
+def cleanUp(oldDir,fileType):
+    oldIds = [(datetime.date.today()-datetime.timedelta(days=i+1)).strftime('%Y%m%d') for i in range(cfg.cleanup)]
+    for id in oldIds:
+        os.makedirs(name=cfg.tmpdir,exist_ok=True)
+        for path in pathlib.Path(oldDir).rglob(pattern=f'*{id}*{fileType}'):
+            os.rename(src=path.__str__(),
+                      dst=path.__str__().replace('\\','/')\
+                                        .replace(oldDir,cfg.tmpdir))
+        if len(os.listdir(path=cfg.tmpdir)): shutil.make_archive(base_name=f'{oldDir}/{id}',
+                                                                 format='zip',
+                                                                 root_dir=cfg.tmpdir)
+        shutil.rmtree(path=cfg.tmpdir)
     pass
 
 def takeCaption():
-    cleanUp(cfg.imgdir)
+    cleanUp(oldDir=cfg.imgdir,
+            fileType=cfg.imgtype)
     caption = cv2.VideoCapture(index=0)
     _,image = caption.read()
     image = cv2.cvtColor(src=image,
-                        code=cv2.COLOR_BGR2GRAY)
+                         code=cv2.COLOR_BGR2GRAY)
     image = cv2.resize(src=image,
-                    dsize=(cfg.imgsize,cfg.imgsize),
-                    fx=0,fy=0,
-                    interpolation=cv2.INTER_AREA)
+                       dsize=(cfg.imgsize,cfg.imgsize),
+                       fx=0,
+                       fy=0,
+                       interpolation=cv2.INTER_AREA)
     image = cv2.filter2D(src=image,
-                        ddepth=-1,
-                        kernel=cfg.kernel)
-    cv2.imwrite(filename=f'{cfg.imgdir}/{cfg.timestamp}.png',
+                         ddepth=-1,
+                         kernel=cfg.kernel)
+    cv2.imwrite(filename=f'{cfg.imgdir}/{cfg.timestamp}.{cfg.imgtype}',
                 img=image)
     pass
-    
+
 def logHomeContents():
-    cleanUp(cfg.recdir)
-    with open(file=f'{cfg.recdir}/{cfg.timestamp}.txt',
+    cleanUp(oldDir=cfg.recdir,
+            fileType=cfg.rectype)
+    with open(file=f'{cfg.recdir}/{cfg.timestamp}.{cfg.rectype}',
               mode='w',
               encoding='utf8') as f:
         for path in pathlib.Path.home().rglob(pattern='*'):
